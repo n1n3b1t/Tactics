b0c14baa10bb0406f7b9e8c8ece8f06aa1fadd36
Valentin Taranenko <n1n3b1t@gmail.com>
2015-09-05 00:22:04 +0300

    Third commit

--- js/actors/hero.js
+++ js/actors/hero.js
@@ -1,14 +1,11 @@
 var Hero = function (game,key) {
   Phaser.Sprite.call(this, game, 0, 0, key);
-  this.width=80;
-  this.height=80;
+  this.width=70;
+  this.height=70;
   this.cellX=0;
   this.cellY=0;
-
-
-
-
-
+  this.stamina=100;
+  this.staminaMax=100;
 
 
 }
@@ -20,10 +17,22 @@ Hero.prototype.select = function (selected) {
     this.tint=selected?0x00f000:0xFFFFFF;
 
 }
+Hero.prototype.moveToCell = function (cell,cells) {
+  // for (var i = 0; i < 8; i++) {
+  //   for (var j = 0; j < 8; j++) {
+  //     cells[j][i]
+  //   }
+  // }
+}
 Hero.prototype.setPositionCell = function (cell) {
     var newX= cell.x+cell.width/2-this.width/2;
     var newY = cell.y+cell.height/2-this.height/2;
     var tween = this.game.add.tween(this);
-    tween.to({x:newX,y:newY},1500,Phaser.Easing.Elastic.InOut);
+    tween.to({x:newX},250);
+    tween.onComplete.add(function () {
+        var tween2 = this.game.add.tween(this);
+        tween2.to({y:newY},250);
+        tween2.start();
+    },this);
     tween.start();
 }
